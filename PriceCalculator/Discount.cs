2e673fc9938188dabcb56090a60486a2d99f0938
Darin Critchlow <darincritchlow@gmail.com>
2019-05-29 21:39:03 -0600

    2. DISCOUNT Add Discount class
    Class to configure discount applied to product

---
+++ PriceCalculator/Discount.cs
@@ -0,0 +1,14 @@
+﻿namespace PriceCalculator
+{
+  public class Discount
+  {
+    public decimal DiscountPercentage { get; }
+
+    public Discount(decimal discountPercentage)
+    {
+      DiscountPercentage = discountPercentage;
+    }
+
+    public override string ToString() => $"{DiscountPercentage}%";
+  }
+}
---
+++ PriceCalculatorTests/DiscountTests.cs
@@ -0,0 +1,15 @@
+﻿using PriceCalculator;
+using Xunit;
+
+namespace PriceCalculatorTests
+{
+  public class DiscountTests
+  {
+    [Fact]
+    public void Discount_GivenPercentage_ReturnsCorrectFormat()
+    {
+      var sut = new Discount(20);
+      Assert.Matches(@"20\%", sut.ToString());
+    }
+  }
+}
